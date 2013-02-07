cd5e3e05504c033672a9a970085c33063b089c0b
Paul Ollivier <paul.ollivier@ullink.com>
2013-02-07 10:39:54 +0100

    Initial Commit

---
+++ houston/houston.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+import sys
+import os
+
+
+class Houston:
+    """Main Houston module"""
+    def __init__(self):
+        pass
+
+    def _load_adapters(self):
+        pass
+
+    def _load_modules(self):
+        pass
+
