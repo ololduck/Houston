5f9e4e304a3a20d7f59c6f4a0bbe28098a1c4cdb
Paul Ollivier <paul.ollivier@ullink.com>
2013-02-07 16:37:09 +0100

    Added some more work.

--- houston/houston.py
+++ houston/houston.py
@@ -3,18 +3,38 @@
 
 import sys
 import os
+import re
+
+import utils.conf
 
 
 class Houston:
     """Main Houston class"""
-    def __init__(self):
-        pass
+    def __init__(self, args):
+        self.adapters = None
+        self.mods = []
+        self.conf = utils.conf.Configuration(args)
+        self._load_modules()
 
     def _load_adapters(self):
         pass
 
     def _load_modules(self):
-        pass
+        regex = re.compile(r'(mod_\w+).py')
+        mods = []
+        for fil in os.listdir('modules'):
+            match = regex.match(fil)
+            if(match is not None):
+                print("found module %s." % match.group(1))
+                mod = __import__('modules.%s' % match.group(1))
+                print mod
+                mods.append(mod)
+                # print(mods[0].Mod)
+        print mods
 
     def start(self):
         pass
+
+
+if __name__ == '__main__':
+    Houston(sys.argv[1:]).start()
--- houston/utils/conf.py
+++ houston/utils/conf.py
@@ -7,6 +7,11 @@ This is the main conf module for Houston. IT checks for values in multiple place
 
 import json
 import os
+import ConfigParser
+
+# TODO: Refactor this class so it is more stream-lined
+
+# FIXME: Only JSON will be available for now
 
 class Configuration:
 
@@ -19,17 +24,25 @@ class Configuration:
         value = self._get_value_cli(key)
         if(value is not None):
             return value
-        value = self._get_value_env(key)
-        if(value is not None):
-            return value
         value = self._get_value_json(key)
         if(value is not None):
             return value
-        value = self._get_value_ini(key)
+        value = self._get_value_env(key)
         if(value is not None):
             return value
+        # value = self._get_value_ini(key)
+        # if(value is not None):
+        #     return value
         return None
+    def save(self, to="json", filename="houston.json"):
+        if(to == "json"):
+            self._save_to_json(filename)
+        elif(to == "ini"):
+            raise NotImplementedError
 
+    def _save_to_json(self, file_name):
+        with open(file_name, 'w+') as f:
+            f.write(json.dumps(self.json))
 
     def _get_value_json(self, key):
         if(self.json is not None):
@@ -47,14 +60,18 @@ class Configuration:
 
     def _get_value_cli(self, key):
 
-        # TODO: use argparse
+        # TODO: use argparse - or not
 
         # if(self.args is not None):
         #     i = 1
         #     for arg in self.args:
         #         if(arg == '--%s' % key)
+        #             if(i + 1 <= len(args) and args[i+1][0:2] != "--"):
+        #                 return self.args[i+1]
+        #         i = i + 1
+        return None
 
-    def _parse_json_file(self, file_name):
+    def _parse_json_file(self, file_name="houston.json"):
         try:
             data = ""
             with open(file_name, 'r') as f:
@@ -62,9 +79,14 @@ class Configuration:
             self.json = json.loads(data)
             if( self.get_value("debug") is not None and self.get_value("verbosity") >=1):
                 print("JSON config from file %s:\n%s" % (file_name, json.dumps(self.json)))
-
         except e:
-            self.json = False
+            self.json = None
 
-    def _parse_ini_file(self, file):
-        pass
+    def _parse_ini_file(self, file_name="houston.ini"):
+        raise NotImplementedError
+        try:
+            cfgp = ConfigParser.SafeConfigParser(None)
+            cfgp.read(file_name)
+            self.ini = cfeg
+        except e:
+            self.ini = None
